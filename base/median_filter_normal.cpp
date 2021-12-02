b9f203cffe8d0e0ff68aed0caa0ce4a1e924d238
Kinga Bober <plgkbober@login01.prometheus.cyfronet.pl>
2021-12-02 19:40:21 +0100

    fixinf bug with median filtering

--- base/median_filter_normal.cpp
+++ base/median_filter_normal.cpp
@@ -15,16 +15,14 @@ int main(int argc, char** argv)
     //     std::cout << "No path to image" << std::endl;
     // }
 
-    cv::Mat img, Bands[3];
-    img = cv::imread("lena_noise.png");
+    cv::Mat img;
+    img = cv::imread("lena_noise3.png");
 
     if(img.empty())
     {
         std::cout << "Could not open or find the image" << std::endl;
         return 1;
     }
-
-    cv::split(img, Bands);
 
     unsigned int window_rows{3};
     unsigned int window_cols{3};
@@ -37,42 +35,59 @@ int main(int argc, char** argv)
 
     std::cout << "channels: " << channels << std::endl;
     uchar *image_uchar = img.data;
+    std::cout << (int)img.data[width * channels * 0 + channels * 2 + 0 ] << "\t" << width * channels * 0 + channels * 2 + 0  << std::endl;
+    std::cout << (int)img.data[width * channels * 0 + channels * 2 + 1 ] << "\t" << width * channels * 0 + channels * 2 + 1  << std::endl;
+    std::cout << (int)img.data[width * channels * 0 + channels * 2 + 2 ] << "\t" << width * channels * 0 + channels * 2 + 2  << std::endl;
     // uchar temp[window_rows * window_cols] = {};
+
+    bool flag = true;
 
     for(size_t row = window_rows / 2; row < height - window_rows / 2; ++row)
     {
         for(size_t col = window_cols / 2; col < width - window_cols / 2; ++col)
         {
             // std::memset(temp, 0, window_rows * window_cols);
+            // uchar temp[window_rows * window_cols] = {0};
+            for(size_t ch = 0; ch < channels; ++ch)
+            {
                 uchar temp[window_rows * window_cols] = {0};
-                for(size_t ch = 0; ch < channels; ++ch)
+                 for(size_t x = 0; x < window_rows; ++x)
                 {
-                     for(size_t x = 0; x < window_rows; ++x)
-                    {
                     for(size_t y = 0; y < window_cols; ++y)
                     {
-
-                        temp[x * window_rows + y] = image_uchar[row * width * channels + col * channels + x - 1 + y - 1 + ch];
+                        temp[x * window_cols + y] = image_uchar[(row + x - 1) * width * channels + (col + y - 1) * channels + ch];
                     }
-                    // std::cout << temp << std::endl;
-                    for(size_t i = 0; i < window_rows * window_cols; ++i)
+                }
+                    // std::cout << std::setw(4) << (unsigned int)temp << std::endl;
+
+                for(size_t i = 0; i < window_rows * window_cols && flag; ++i)
+                {
+                    std::cout << std::setw(4) << (int) temp[i];
+                }
+                if(flag) std::cout << std::endl;
+                for(size_t i = 0; i < window_rows * window_cols; ++i)
+                {
+                    for(size_t j = i + 1; j < window_rows * window_cols; ++j)
                     {
-                        for(size_t j = i + 1; j < window_rows * window_cols; ++j)
+                        if(temp[i] > temp[j])
                         {
-                            if(temp[i] > temp[j])
-                            {
-                                uchar tmp = temp[i];
-                                temp[i] = temp[j];
-                                temp[i] = tmp; 
-                            }
+                            uchar tmp = temp[i];
+                            temp[i] = temp[j];
+                            temp[j] = tmp; 
                         }
                     }
-                    image_uchar[row * width * channels + col * channels + ch] = temp[1 + (window_rows * window_cols) / 2];
                 }
+
+                for(size_t i = 0; i < window_rows * window_cols && flag; ++i)
+                {
+                    std::cout << std::setw(4) << (int) temp[i];
+                }
+                flag = false;
+                image_uchar[row * width * channels + col * channels + ch] = temp[(window_rows * window_cols) / 2];
             }
         }
     }
-
+    std::cout << std::endl;
     // std::cout << "format: " << imageFormat << " width: " << width << " height: " << height << std::endl;  
     std::cout << "width: " << img.cols << " height: " << img.rows << std::endl; 
     std::cout << "argc: " << argc << " argv: ";
@@ -82,6 +97,8 @@ int main(int argc, char** argv)
     }
     std::cout << std::endl;
 
-    cv::imwrite("lena_denoised.png", img);
+    cv::Mat output_image = cv::Mat(height, width, img.type(), image_uchar);
+
+    cv::imwrite("lena_denoised.png", output_image);
     return 0;
 }
