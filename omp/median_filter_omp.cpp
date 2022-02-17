fbe979ce4a52068b7b6f3dcac829cb890d3742f0
Albert Jakieła <albertjakiela@gmail.com>
2022-02-17 18:41:40 +0100

    fixing problem with segmentation fault

--- omp/median_filter_omp.cpp
+++ omp/median_filter_omp.cpp
@@ -7,26 +7,17 @@
 #include "opencv2/core.hpp"
 #include "opencv2/imgcodecs.hpp"
 
-void bubbleSort(uchar* buffer, size_t bufferSize)
-{
-    
-    for(size_t i = 0; i <bufferSize; ++i){
-        for(size_t j = i + 1; j < bufferSize; ++j){
-            if(buffer[i] > buffer[j]){
-                uchar tmp = buffer[i];
-                buffer[i] = buffer[j];
-                buffer[j] = tmp; 
-            }
-        }
-    }
-}
+
+#define NUM_THREADS 8
+#define CHUNK 16000
 
 int main(int argc, char** argv)
 {
-    if(argc >= 4)
+
+    if(argc >= 2)
     {
         cv::Mat img;
-        img = cv::imread(argv[3]);
+        img = cv::imread(argv[1]);
 
         if(img.empty())
         {
@@ -34,53 +25,74 @@ int main(int argc, char** argv)
             return -1;
         }
 
-        unsigned int window_rows{std::stoul(argv[1])};
-        unsigned int window_cols{std::stoul(argv[2])};
+        unsigned int window_rows{3};
+        unsigned int window_cols{3};
 
         int width = img.cols;
         int height = img.rows;
         int channels = img.channels();
 
+        size_t imageSize = width * height * channels;
+
         uchar *image_uchar = img.data;
-        uchar temp[window_rows * window_cols] = {0};
+
+        uchar window[9];
+
+        size_t windowSize = window_cols * window_rows;
 
         size_t row = 0;
         size_t col = 0;
         size_t ch = 0;
         size_t x = 0;
         size_t y = 0;
+        int minval;
         auto start = std::chrono::steady_clock::now();
-        #pragma omp parallel private(row, col, ch, temp) 
+
+        omp_set_num_threads(NUM_THREADS);
+        #pragma omp parallel private(window, row, col, ch, x, y, minval)
         {
-            #pragma omp for schedule(dynamic, 1)
+            #pragma omp parallel for 
             for(row = window_rows / 2; row < height - window_rows / 2; ++row){
                 for(col = window_cols / 2; col < width - window_cols / 2; ++col){
                     for(ch = 0; ch < channels; ++ch){
-                        // fill window with data from input image
-                        for(x = 0; x < window_rows; ++x){
-                            for(y = 0; y < window_cols; ++y){
-                                temp[x * window_cols + y] = image_uchar[(row + x - 1) * width * channels + (col + y - 1) * channels + ch];
+                    // fill window with data from input image
+                        for(y = 0; y < window_rows; ++y){
+                            for(x = 0; x < window_cols; ++x){
+                                window[y * window_cols + x] = image_uchar[(row + y - (window_rows / 2) ) * width * channels + (col + x - (window_cols / 2)) * channels + ch];
                             }
                         }
-                        // bubble sort pixel window
-                        bubbleSort(temp, window_rows * window_cols);
-                        image_uchar[row * width * channels + col * channels + ch] = temp[(window_rows * window_cols) / 2];
+                        
+                        for (int i = 0; i < (1 + (windowSize / 2)); ++i) {
+                            // --- Find the position of the minimum element
+                            int minval = i;
+                            for (int l = i + 1; l < (windowSize); ++l) 
+                            {
+                                if (window[l] < window[minval]) minval=l;
+                            }
+                            // --- Put found minimum element in its place
+                            uchar temp = window[i];
+                            window[i] = window[minval];
+                            window[minval] = temp;
+                        }
+                        image_uchar[row * width * channels + col * channels + ch] = window[4];
                     }
                 }
             }
         }
+
         auto end = std::chrono::steady_clock::now();
         std::cout << "Elapsed time in milliseconds: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
         cv::Mat output_image = cv::Mat(height, width, img.type(), image_uchar);
 
         if(5 == argc)
         {
-            cv::imwrite(argv[4], output_image);
+            cv::imwrite(argv[2], output_image);
         }
         else
         {
             cv::imwrite("output.png", output_image);
         }
+        
         return 0;
     }
     else
@@ -88,8 +100,6 @@ int main(int argc, char** argv)
         std::cout << "usage: median_filter_normal wh ww input_image <output_directory>" << std::endl << std::endl;
         std::cout << "Applies median filter to the image" << std::endl << std::endl;
         std::cout << "Positional arguments:" << std::endl;
-        std::cout << std::left << std::setw(30) << "  wh" << "kernel height, odd number" << std::endl;
-        std::cout << std::left << std::setw(30) << "  ww" << "kernel width, odd number" << std::endl;
         std::cout << std::left << std::setw(30) << "  input_image" << "input image directory" << std::endl;
         std::cout << std::left << std::setw(30) << "  <output_directory>" << "optional output directory, if not present then image is saved to output.png" << std::endl;
         return -1;
--- omp/run_median_filter_omp.sh
+++ omp/run_median_filter_omp.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -l
-#SBATCH -J median_filter_job
+#SBATCH -J median_filter_job_omp
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=8
 #SBATCH --time=00:10:00
 #SBATCH -p plgrid-testing
-#SBATCH --output=median_filter.out
-cd $HOME/median-filter/base
+#SBATCH --output=omp_median_filter.out
+cd $HOME/median-filter/omp
 module -q add plgrid/libs/opencv
 module -q add plgrid/tools/intel/2021.3.0
-sleep 10
 make
-./median_filter_normal
+./median_filter_omp ../lena_noise1.png
